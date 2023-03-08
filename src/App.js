24e8818260fd4e788b6e1574a5a7b59ec7f06d84
riyas.r <riyas.r@hcl.com>
2023-03-08 21:14:56 +0530

    react-draggable-slide lib code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+import Slider from "./components/Slider";
+import { projectList } from "./app/data";
+
+const sliderSettings = {
+  data: projectList,
+  speed: 1200,
+  easing: "expo",
+  bgColor: "rgba(255, 255, 255, 0.05)",
+  buttonHref: "https://www.google.com",
+  buttonTarget: "_self",
+  buttonText: "View project",
+  showButton: true,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper id="projects">
+    <Slider sliderSettings={sliderSettings} />
+  </Wrapper>
   );
 }
+
+const Wrapper = styled.section`
+  width: auto;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
 
 export default App;
